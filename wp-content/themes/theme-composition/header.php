eb5d8c2d6201cd5f819443485e0cfae16787870e
Avanhellemont <vanhellemont.aurelie@gmail.com>
2023-01-23 14:47:37 +0100

    add file sass - 23/01/23 - 14:48

--- wp-content/themes/theme-composition/header.php
+++ wp-content/themes/theme-composition/header.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head() ?>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
 </head>
-
+<header>
+<img class="logo-icon" src="<?php bloginfo('template_url'); ?>/assets/svg/logo-full.svg" alt="logo">
+    <nav>
+    <?php wp_nav_menu(array( 'theme_location' => 'menu_principal' )) ?>
+    </nav>
+    <a href="#" class="header__button">Wishlist</a>
+</header>
 <body>
