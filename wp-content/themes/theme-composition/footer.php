653ef4b709219b9ac28106adc5c1f89901c754d7
hanaelle <hananeelkarouni1@gmail.com>
2023-01-24 16:03:36 +0100

    fin de footer

--- wp-content/themes/theme-composition/footer.php
+++ wp-content/themes/theme-composition/footer.php
@@ -1,27 +1,34 @@
+<footer class="footer">
+<!-------------------------------GOOGLE MAP------------------------------------>
+
+<hr class="footer__hr">
+<div class="div1">
+        <h2 class="footer__title">Rendez-nous visite !</h2>
+            <a class="google__map" href="#"> <img src="<?php bloginfo('template_url'); ?>/assets/img/svg/Group 1300.svg" alt="">Google map </a>
+    </div>
+<!-------------------------------COORDONNES------------------------------------>
+<div class="div2">
+    <p class="footer__adresse"><img src="<?php bloginfo('template_url'); ?>/assets/img/svg/marker-road.svg" alt="">Route de Hamoir, 87 – 4500 Huy </p>
+    <p class="footer__mail"><img src="<?php bloginfo('template_url'); ?>/assets/img/svg/icon-arrobas.svg" alt=""><a href="#">info@meubles-composition.be</a></p>
+    <p class="footer__phone"><img src="<?php bloginfo('template_url'); ?>/assets/img/svg/phone-2.svg" alt=""><a href="">+32 (0)85 21 52 38</a> </p>
 </div>
-    <footer>
+<!-------------------------------LIENS SOCIAUX------------------------------------>
 
-
-<h4>Rendez-nous visite !</h4>
-        <a class="google-map" href="#">Google map <img src="<?php bloginfo('template_url'); ?>/assets/img/svg/Group 1300.svg" alt=""></a>
+<div class="div3">
+    <div class="justIcons">
+        <div class="footer__facebook"><a href="#" target="_blank"><img src="<?php bloginfo('template_url'); ?>/assets/svg/logo-facebook.svg" alt="logo-facebook"> </a> </div>
+        <div class="footer__insta"><a href="#" target="_blank"><img src="<?php bloginfo('template_url'); ?>/assets/svg/logo-instagram.svg" alt="logo-instagram"> </a> </div>
     </div>
+    <h6>
+        <p class="footer__copy">
+            <a href="#">&#169 COMPOSITION 2019 - </a>
+            <a href="#"> POLITIQUE DE CONFIDENTIALITE</a>
+        </p>
+    </h6>
 
-    <!-- <div class="adress col-lg-4"> -->
-        <p><img src="<?php bloginfo('template_url'); ?>/assets/img/svg/marker-road.svg" alt="">Route de Hamoir, 87 – 4500 Huy </p>
-        <p><img src="<?php bloginfo('template_url'); ?>/assets/img/svg/icon-arrobas.svg" alt=""><a href="#">info@meubles-composition.be</a></p>
-        <p><img src="<?php bloginfo('template_url'); ?>/assets/img/svg/phone-2.svg" alt=""><a href="">+32 (0)85 21 52 38</a> </p>
+        <p class="footer__tva">TVA : BE0872574089</p>
     </div>
-    <!--<div class="sm-icons col-lg-4"> -->
-        <div class="justIcons">
-            <span><a href="#" target="_blank"><img src="<?php bloginfo('template_url'); ?>/assets/svg/logo-facebook.svg" alt=""> </a> </span>
-            <span><a href="#"target="_blank"><img src="<?php bloginfo('template_url'); ?>/assets/svg/logo-instagram.svg" alt=""> </a> </span>
-        </div>
-        <h6><p><a href="#">&#169 COMPOSITION 2019 - </a><a href="#"> POLITIQUE DE CONFIDENTIALITE</a> </p></h6>
-        <p>TVA : BE0872574089</p>
-    </div>
-</div>
-
-    </footer>
+</footer>
     <?php wp_footer() ?> 
 </body>
 </html>
