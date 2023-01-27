b72393d7548080254d4ea6b833b532b72095ed21
hanaelle <hananeelkarouni1@gmail.com>
2023-01-27 12:10:27 +0100

    27.01.23   12.10

--- wp-content/themes/theme-composition/footer.php
+++ wp-content/themes/theme-composition/footer.php
@@ -1,34 +1,36 @@
 <footer class="footer__container">
-<!-------------------------------1 er PARTIE----------------------------------->
+<!-------------------------------   1 er PARTIE    ----------------------------------->
 
 <div class="footer__top__content">
-    <div class="footer__content">        <img src="<?php bloginfo('template_url'); ?>/assets/images/1500x150_02.jpg" alt=""></a>
+    <div class="footer__content"><img src="<?php bloginfo('template_url'); ?>/assets/images/1500x150_02.jpg" alt="">
 
         <img class="footer__content__img" src="" alt="">
-        <p class="footer__content__text" >Tissus et habillage maison </p>
+        <p class="footer__content__text" href="#">Tissus et habillage maison <img src="<?php bloginfo('template_url'); ?>/assets/svg/Group 1300.svg" alt=""></p>
+
     </div>
 
-    <!-------------------------------  HORAIRE  ----------------------------------->
+    <!-------------------------------    HORAIRE    ----------------------------------->
 
     <div class="footer__horaire"></div>
     <div></div>
 
 </div>
-<!-------------------------------2 eme PARTIE------------------------------------>
+<!-------------------------------   2 eme PARTIE   ------------------------------------>
 
-<!-------------------------------GOOGLE MAP------------------------------------>
+<!-------------------------------   GOOGLE MAP   -------------------------------------->
 
 <div class="div1">
         <h2 class="footer__title">Rendez-nous visite !</h2>
         <a class="google__map" href="#">Google map <img src="<?php bloginfo('template_url'); ?>/assets/svg/Group 1300.svg" alt=""></a>
     </div>
-<!-------------------------------COORDONNES------------------------------------>
+<!-------------------------------   COORDONNES   ------------------------------------>
 <div class="div2">
     <div class="footer__container__icon__adress"><img class="footer__img__adress" src="<?php bloginfo('template_url'); ?>/assets/svg/marker-road.svg" alt=""><p class="footer__adress">Route de Hamoir, 87 – 4500 Huy </p></div> 
     <p class="footer__mail">  <img class="footer__img__mail" src="<?php bloginfo('template_url'); ?>/assets/svg/icon-arrobas.svg" alt=""><a class="footer__info" href="#">info@meubles-composition.be</a></p>
     <p class="footer__phone"> <img class="footer__img__phone" src="<?php bloginfo('template_url'); ?>/assets/svg/phone-2.svg" alt=""><a class="footer__tel" href="">+32 (0)85 21 52 38</a> </p>
 </div>
-<!-------------------------------LIENS SOCIAUX------------------------------------>
+
+<!-------------------------------   LIENS SOCIAUX   ------------------------------------>
 
 <div class="div3">
     <div class="footer__icons">
