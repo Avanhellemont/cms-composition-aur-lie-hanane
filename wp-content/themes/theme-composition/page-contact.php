deb0760e4ef2cfa6649a183a9768cfc5088bb315
hanaelle <hananeelkarouni1@gmail.com>
2023-02-01 12:32:39 +0100

    01.01 12h30 page contact

---
+++ wp-content/themes/theme-composition/page-contact.php
@@ -0,0 +1,16 @@
+<?php wp_header();?>
+<main>
+    <?php $contact = get_field('contact'); if
+    ?>
+
+
+    <div class="contact__lieux">
+
+    </div>
+
+    <div class class="contact__form">
+
+    </div>
+    
+</main>
+<?php wp_footer();?>
