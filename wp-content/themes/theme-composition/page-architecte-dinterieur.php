b01414b7e389db6218feaedb31e56c0f22b5be05
Avanhellemont <vanhellemont.aurelie@gmail.com>
2023-02-02 09:50:06 +0100

    commit changes 02-02-23 09:49

--- wp-content/themes/theme-composition/page-architecte-dinterieur.php
+++ wp-content/themes/theme-composition/page-architecte-dinterieur.php
@@ -24,7 +24,7 @@ if( $etude_archi['etude_titre'] && $etude_archi['etude_soustitre'] && $etude_arc
     <div class="etude__content--left">
         <h2 class="etude__titre"><?php echo $etude_archi['etude_titre']; ?></h2>
         <p class="etude__sous-titre"><?php echo $etude_archi['etude_soustitre']; ?></p>
-        <p class="etude__text"><?php echo $etude_archi['etude_texte']; ?></p>
+        <p><?php echo $etude_archi['etude_texte']; ?></p>
     </div>
     <div class="etude__content--right">
         <div class="etude__liste"><?php echo $etude_archi['etude_liste']; ?></div>
@@ -39,10 +39,12 @@ $stephan = get_field('stephan');
 if( $stephan['stephan_image'] && $stephan['stephan_titre'] && $stephan['stephan_soustitre'] && $stephan['stephan_texte']):
 ?>
  <section class="stephan">
-    <img class="stephan_image" src="<?php echo $stephan['stephan_image']['sizes']['taille_slide']; ?>">
-    <h2 class="stephan__titre"><?php echo $stephan['stephan_titre']; ?></h2>
-    <p class="stephan__sous-titre"><?php echo $stephan['stephan_soustitre']; ?></p>
-    <p class="stephan__texte"><?php echo $stephan['stephan_texte']; ?></p>
+    <img class="stephan__image" src="<?php echo $stephan['stephan_image']['sizes']['taille_slide']; ?>">
+    <div class="stephan__content--right">
+        <h2 class="stephan__titre"><?php echo $stephan['stephan_titre']; ?></h2>
+        <p class="stephan__sous-titre"><?php echo $stephan['stephan_soustitre']; ?></p>
+        <p class="stephan__texte"><?php echo $stephan['stephan_texte']; ?></p>
+    </div>
 </section>
     <?php endif; ?>
 
@@ -51,16 +53,18 @@ if( $stephan['stephan_image'] && $stephan['stephan_titre'] && $stephan['stephan_
 $tissus = get_field('tissus');
 if( $tissus['tissus_image'] && $tissus['tissus_titre'] && $tissus['tissus_soustitre'] && $tissus['tissus_texte']):
 ?>
- <section class="tissus">
-    <img class="tissus_image" src="<?php echo $tissus['tissus_image']['sizes']['taille_slide']; ?>">
-    <h2 class="tissus__titre"><?php echo $tissus['tissus_titre']; ?></h2>
-    <p class="tissus__sous-titre"><?php echo $tissus['tissus_soustitre']; ?></p>
-    <p class="tissus__texte"><?php echo $tissus['tissus_texte']; ?></p>
-</section>
+ <section class="tissus-artisanat">
+    <div class="tissus__content--right">
+        <img class="tissus__image" src="<?php echo $tissus['tissus_image']['sizes']['taille_slide']; ?>">
+        <h2 class="tissus__titre"><?php echo $tissus['tissus_titre']; ?></h2>
+        <p class="tissus__sous-titre"><?php echo $tissus['tissus_soustitre']; ?></p>
+        <p class="tissus__texte"><?php echo $tissus['tissus_texte']; ?></p>
+    </div>
+    </section>
     <?php endif; ?>
 
 </main>
-<?php get_footer() ?>
+
 
 
 
