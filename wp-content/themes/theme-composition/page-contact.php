983dd79aa19e5b5a0d57e66f28f71557c5972d04
hanaelle <hananeelkarouni1@gmail.com>
2023-02-02 16:10:24 +0100

    02.02 16h10 top page contact

--- wp-content/themes/theme-composition/page-contact.php
+++ wp-content/themes/theme-composition/page-contact.php
@@ -1,51 +1,49 @@
 <?php get_header();?>
 
-
-<main>
+<main class="page__contact">
     <?php $contact = get_field('contact');?>
-
-<div class="contact__formulaire__contact">
-    <!------------------------------- GOOGLE  ----------------------------------->
-    <div class="contact_lieux">
-        <a class="google__map" href="#">Google map <img src="<?php bloginfo('template_url'); ?>/assets/svg/Group 1300.svg" alt=""></a>
-    </div>
-<!-------------------------------   Adresse ------------------------------------>
-<div class="div2">
-    <div class="footer__container__icon__adress"><img class="footer__img__adress" src="<?php bloginfo('template_url'); ?>/assets/svg/marker-road.svg" alt=""><p class="footer__adress">Route de Hamoir, 87 – 4500 Huy </p></div> 
-    <p class="footer__mail">  <img class="footer__img__mail" src="<?php bloginfo('template_url'); ?>/assets/svg/icon-arrobas.svg" alt=""><a class="footer__info" href="#">info@meubles-composition.be</a></p>
-    <p class="footer__phone"> <img class="footer__img__phone" src="<?php bloginfo('template_url'); ?>/assets/svg/phone-2.svg" alt=""><a class="footer__tel" href="">+32 (0)85 21 52 38</a> </p>
-</div>
-
-    <div class="contact__content">
-         <div class="contact__text__bloc">
-                <a class="contact__content__text" href="#">Contact
-                    <img class="contact__content__img" src="<?php bloginfo('template_url'); ?>/assets/svg/Group 1300.svg" alt="icon">
-                </a>
+<div class="contact">    
+<!-------------------------------------- BANNER TOP -->
+<div class="banner__top">
+    <div class="container__banner">
+        <div class="content">
+            <div class="titre__container">
+                <div class="trait"> - </div>
+                    <h1 class="titre">Contact</h1>
             </div>
         </div>
     </div>
+<!----------------------------------------Bloc coordonnes-------->
+<div class="bloc__coordonnes">
+    
+    <div class="contact__container__icon__adress"><img class="contact__img__adress" src="<?php bloginfo('template_url'); ?>/assets/svg/marker-road.svg" alt=""><p class="contact__adress">Route de Hamoir, 87 – 4500 Huy </p></div> 
+    <p class="contact__mail">  <img class="contact__img__mail" src="<?php bloginfo('template_url'); ?>/assets/svg/icon-arrobas.svg" alt=""><a class="contact__info" href="#">info@meubles-composition.be</a></p>
+    <p class="footer__phone"> <img class="contact__img__phone" src="<?php bloginfo('template_url'); ?>/assets/svg/phone-2.svg" alt=""><a class="contact__tel" href="">+32 (0)85 21 52 38</a> </p>
 
-    <!-------------------------------    HORAIRE    ----------------------------------->
+</div>
+<!--------------------------------------Bloc horaire-------->
 
-    <div class="footer__horaire">
-        <div class="footer__bloc__heure">
-            <p class="footer__icon"><img class="footer__img__time" src="<?php bloginfo('template_url'); ?>/assets/svg/time-clock.svg" alt=""></p>
-            <div class="footer__horaire">
-                <h5>Horaire</h5>
-                    <ul>
-                        <li>lundi > dimanche : (Fermé le mardi)</li>
-                        <li>10h > 18h</li>
-                    </ul>
+
+<div class="container__horaire">
+    <div class="contact__bloc__heure">
+        <p class="contact__icon">
+            <img class="contact__img__time" src="<?php bloginfo('template_url'); ?>/assets/svg/time-clock.svg" alt=""></p>
+                <div class="contact__horaire">
+                    <h5>Horaire</h5>
+                        <ul>
+                            <li>lundi > dimanche : (Fermé le mardi)</li>
+                            <li>10h > 18h</li>
+                        </ul>
             </div>
-        </div>
     </div>
-    
+</div>
+</div>
 
-</div>
- <div class class="contact__form">
-<p>le formulaire de contact</p>
-    </div>
-</div>
-    
+<!-------------------------------------- BANNER medium -->
+    <div class="banner__medium"></div>
+<!------------------------------------ BANNER Bottom -->
+    <div class="banner__bottom-call-image"></div>
+
+</div>    
 </main>
 <?php get_footer();?>
