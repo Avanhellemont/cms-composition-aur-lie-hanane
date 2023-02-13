0a0c694740a3ccd669450bd2fd0c79fb3dddc743
hanaelle <hananeelkarouni1@gmail.com>
2023-02-13 16:58:10 +0100

    13.02.23 16h58

---
+++ wp-content/themes/theme-composition/archive.php
@@ -0,0 +1,47 @@
+<?php get_header();?>
+
+<main class="page__archive">
+
+<div class="archive">
+    <div class="banner__top">
+        <div class="banner__content">
+            <div class="content">
+                <div class="titre__container"></div>
+                <div class="trait"> - </div>
+                <h1 class="titre">Architecte d’intérieur en action</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--Banner liste -->
+<div class="banner__list">
+    <div class="banner__content">
+        <div class="banner__content">
+            <ul>
+                <li class="inside">
+                    <a href="">
+                        <div class="image__container">
+                            <div class="image"></div>
+                        </div>
+                        <div class="text__container"></div>
+                    </a> 
+                </li>
+
+
+
+
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+
+</div>
+
+
+</main>
+<?php get_footer();?>
