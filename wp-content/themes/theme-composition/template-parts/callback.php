637fd016d5a5d8447372eb4ad6bf23b354ca7452
hanaelle <hananeelkarouni1@gmail.com>
2023-02-13 12:34:18 +0100

    13.02.23  termine la template part

--- wp-content/themes/theme-composition/template-parts/callback.php
+++ wp-content/themes/theme-composition/template-parts/callback.php
@@ -1,13 +1,18 @@
+<div class="banner__call--action">
 
+<!--    Banniere photo   -->
+        <div  class="template__banner--image" >
+            <img class="medium__content__img" src="<?php bloginfo('template_url'); ?>/assets/images/2600x700_01.jpg" alt="icon">
+        </div>
 
-<div class="template__banner">
-    <div  class="template__banner--image" >
-
-        <h1>ici ce trouvera la banniere sur toutes les pages</h1>
-        <img class="medium__content__img" src="<?php bloginfo('template_url'); ?>/assets/images/2600x700_01.jpg" alt="icon">
-
-        <p class="template__banner--text"></p>
-        <p class="template__content"> 10% pour vos prochains achat ? </p>
-        <p>Ma wichlist</p>
+<!--    Bloc promo   -->    
+    <div class="template__banner">    
+            <div class="template__container--promo">
+                <div class="template__banner--text">
+                    <div class="template__content"> 10% pour vos prochains achat ? </div>
+                    <div class="template__fleche">Ma wichlist</div>
+                </div>
+            </div>
     </div>
 </div>
+
