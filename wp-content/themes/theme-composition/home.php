69acbc9eaeebcd9229c4b86e71cc02b3b5a785b0
hanaelle <hananeelkarouni1@gmail.com>
2023-02-16 10:41:17 +0100

    16.02.23  10.41

--- wp-content/themes/theme-composition/home.php
+++ wp-content/themes/theme-composition/home.php
@@ -6,10 +6,11 @@
     <div class="banner__top">
         <div class="banner__content">
             <div class="content">
-                <div class="titre__container"></div>
-                <div class="trait"> - </div>
-            <!--    <h1 class="titre">Architecte d’intérieur en action</h1>   -->
-            </div>
+                <div class="titre__container">
+                    <div class="trait"> - </div>
+                        <h1 class="titre">Architecte d’intérieur en action</h1>   
+                    </div>
+                </div>
         </div>
     </div>
 </div>
@@ -18,12 +19,12 @@
 <div class="banner__list">
     <div class="banner__content">
         <div class="banner__content">
-            <ul>
+            <ul class="ul">
                 <li class="inside">
                     <a href="">
                         <div class="image__container">
                             <div class="image">
-                                <img src="<?php bloginfo('template_url'); ?>/assets/images/1350x750_03.jpg" alt="">>
+                           <!--      <img src="<?php bloginfo('template_url'); ?>/assets/images/1350x750_03.jpg" alt=""> -->
                             </div>
                         </div>
                         <div class="text__container"></div>
